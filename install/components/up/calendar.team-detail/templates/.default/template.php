7c70e3be6c7b7dbbd07331fc7ad2adc803f55382
Popova Natalya <popova.natashar@gmail.com>
2023-04-15 17:03:56 +0200

    Верстка страницы группы

--- install/components/up/calendar.team-detail/templates/.default/template.php
+++ install/components/up/calendar.team-detail/templates/.default/template.php
@@ -1,5 +1,7 @@
 <section class="container">
+
 <div class="columns is-multiline">
+
 	<div class="column has-text-centered">
 
 		<div class="card">
@@ -13,28 +15,42 @@
 				<h1 class="title is-4">34</h1>
 			</div>
 		</div>
-
-		<div class="columns">
-			<div class="column is-four-fifths">is-four-fifths</div>
-			<div class="column">Auto</div>
-			<div class="column">Auto</div>
-		</div>
-
-		<div class="block">
-				<a class="group-card block" href="/group/1/">
-					<div class="block group">
-						<div>
-							<figure class="image is-64x64">
-								<img class="is-rounded" src="https://bulma.io/images/placeholders/64x64.png">
-							</figure>
-						</div>
-						<p style="margin-left: 15px;">Имя Фамилия</p>
-					</div>
-				</a>
-		</div>
-
 	</div>
-	<div class="column is-half">rfty</div>
-	<div class="column">tyu</div>
+
+	<div class="column is-half">
+			<form class="box">
+				<div class="field">
+					<div class="control">
+						<textarea class="textarea" placeholder="Введите сообщение" rows="2"></textarea>
+					</div>
+				</div>
+				<button class="button is-primary">Отправить</button>
+			</form>
+	</div>
+
+	<div class="column">
+		<input type="text" class='date-input'>
+		<div class="wrapper">
+			<div class="level header is-marginless">
+				<span class="left-arrow"><</span>
+				<span class="year-month"></span>
+				<span class="right-arrow">></span>
+			</div>
+
+			<div class="date-wrapper">
+				<div class="level-left day-nums">
+					<span class='date-item'>Sun</span>
+					<span class='date-item'>Mon</span>
+					<span class='date-item'>Tue</span>
+					<span class='date-item'>Wen</span>
+					<span class='date-item'>Thu</span>
+					<span class='date-item'>Fri</span>
+					<span class='date-item'>Sat</span>
+				</div>
+				<div class="dates">
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
 </section>
