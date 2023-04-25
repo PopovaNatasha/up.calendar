9284d44282292e34ec6e3a1103b2fdf03a09f609
Gleb <catinhat001@gmail.com>
2023-04-25 19:42:29 +0200

    корректное отображение приватных групп и ссылок вступить

--- install/components/up/calendar.team-detail/class.php
+++ install/components/up/calendar.team-detail/class.php
@@ -32,16 +32,35 @@ class CalendarCalendarComponent extends CBitrixComponent
 
 	protected function getTeam()
 	{
-		$request = Application::getInstance()->getContext()->getRequest();
+        global $USER;
+        $request = Application::getInstance()->getContext()->getRequest();
 		if ($request->get('id'))
 		{
 			$idTeam = (int)$request->get('id');
 			$team = Calendar::getTeamById($idTeam);
+            $participants = Calendar::getParticipantsTeam($idTeam);
+
+            if ($team['IS_PRIVATE'] === '1')
+            {
+                $check = 0;
+                foreach ($participants as $participant)
+                {
+                    if ($participant['ID_USER'] === $USER->getID())
+                    {
+                        $check = 1;
+                    }
+                }
+                if ($check === 0)
+                {
+                    LocalRedirect('/404');
+                }
+            }
+
             if(!$team['INVITE_LINK'])
             {
                 $team['INVITE_LINK'] = Calendar::createInviteLink($idTeam);
             }
-			$participants = Calendar::getParticipantsTeam($idTeam);
+
 			$this->arResult = $team;
 			$this->arResult['PARTICIPANTS'] = $participants;
 		}
