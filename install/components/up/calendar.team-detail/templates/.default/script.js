b4a46f1d579d3aa6a55fa16b3a7d327fd5c362be
Popova Natalya <popova.natashar@gmail.com>
2023-04-29 23:12:48 +0200

    Починила форму создания события

--- install/components/up/calendar.team-detail/templates/.default/script.js
+++ install/components/up/calendar.team-detail/templates/.default/script.js
@@ -1,52 +1,3 @@
-
-document.addEventListener('DOMContentLoaded', () => {
-	// Functions to open and close a modal
-	function openModal($el)
-	{
-		$el.classList.add('is-active');
-	}
-	function closeModal($el)
-	{
-		$el.classList.remove('is-active');
-	}
-
-	function closeAllModals()
-	{
-		(document.querySelectorAll('.modal') || []).forEach(($modal) => {
-			closeModal($modal);
-		});
-	}
-
-	// Add a click event on buttons to open a specific modal
-	(document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
-		const modal = $trigger.dataset.target;
-		const $target = document.getElementById(modal);
-
-		$trigger.addEventListener('click', () => {
-			openModal($target);
-		});
-	});
-
-	// Add a click event on various child elements to close the parent modal
-	(document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
-		const $target = $close.closest('.modal');
-
-		$close.addEventListener('click', () => {
-			closeModal($target);
-		});
-	});
-
-	// Add a keyboard event to close all modals
-	document.addEventListener('keydown', (event) => {
-		const e = event || window.event;
-
-		if (e.keyCode === 27)
-		{ // Escape key
-			closeAllModals();
-		}
-	});
-});
-
 function generateLink(link, idTeam)
 {
 	const input = document.getElementById('InviteLink');
@@ -70,9 +21,9 @@ function generateLink(link, idTeam)
 	});
 
 }
+
 function changeView(evt, tabName)
 {
-	console.log(tabName);
 	var i, tablinks;
 
 	tablinks = document.getElementsByClassName("tab");
@@ -88,7 +39,6 @@ function setRenderRangeText() {
 	var renderRange = document.getElementById('renderRange');
 	let calendar = CalendarEventsList.calendar;
 	moment.locale('ru');
-	console.log(renderRange);
 	var options = calendar.getOptions();
 	var viewName = calendar.getViewName();
 	var html = [];
