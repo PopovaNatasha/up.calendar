38c1586642e6e78c3ac20db23461bf7acf7b4d72
Gleb <catinhat001@gmail.com>
2023-05-03 18:46:07 +0200

    Добавлен компонент изменения данных пользователя, ПОКА не рабочий

--- install/components/up/calendar.profile/templates/.default/template.php
+++ install/components/up/calendar.profile/templates/.default/template.php
@@ -1,1 +1,217 @@
-Тут будет редактирование профиля.
+<?
+/**
+ * @global CMain $APPLICATION
+ * @var array $arParams
+ * @var array $arResult
+ */
+if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true)
+	die();
+
+if($arResult["SHOW_SMS_FIELD"] == true)
+{
+	CJSCore::Init('phone_auth');
+}
+?>
+
+<div class="bx-auth-profile">
+
+<?ShowError($arResult["strProfileError"]);?>
+<?
+if ($arResult['DATA_SAVED'] == 'Y')
+	ShowNote(GetMessage('PROFILE_DATA_SAVED'));
+?>
+
+<?if($arResult["SHOW_SMS_FIELD"] == true):?>
+
+<form method="post" action="<?=$arResult["FORM_TARGET"]?>">
+<?=$arResult["BX_SESSION_CHECK"]?>
+<input type="hidden" name="lang" value="<?=LANG?>" />
+<input type="hidden" name="ID" value=<?=$arResult["ID"]?> />
+<input type="hidden" name="SIGNED_DATA" value="<?=htmlspecialcharsbx($arResult["SIGNED_DATA"])?>" />
+
+
+<p><input type="submit" name="code_submit_button" value="<?echo GetMessage("main_profile_send")?>" /></p>
+
+</form>
+
+<script>
+new BX.PhoneAuth({
+	containerId: 'bx_profile_resend',
+	errorContainerId: 'bx_profile_error',
+	interval: <?=$arResult["PHONE_CODE_RESEND_INTERVAL"]?>,
+	data:
+		<?=CUtil::PhpToJSObject([
+			'signedData' => $arResult["SIGNED_DATA"],
+		])?>,
+	onError:
+		function(response)
+		{
+			var errorDiv = BX('bx_profile_error');
+			var errorNode = BX.findChildByClassName(errorDiv, 'errortext');
+			errorNode.innerHTML = '';
+			for(var i = 0; i < response.errors.length; i++)
+			{
+				errorNode.innerHTML = errorNode.innerHTML + BX.util.htmlspecialchars(response.errors[i].message) + '<br>';
+			}
+			errorDiv.style.display = '';
+		}
+});
+</script>
+
+<div id="bx_profile_error" style="display:none"><?ShowError("error")?></div>
+
+<div id="bx_profile_resend"></div>
+
+<?else:?>
+
+<script type="text/javascript">
+<!--
+var opened_sections = [<?
+$arResult["opened"] = $_COOKIE[$arResult["COOKIE_PREFIX"]."_user_profile_open"];
+$arResult["opened"] = preg_replace("/[^a-z0-9_,]/i", "", $arResult["opened"]);
+if ($arResult["opened"] <> '')
+{
+	echo "'".implode("', '", explode(",", $arResult["opened"]))."'";
+}
+else
+{
+	$arResult["opened"] = "reg";
+	echo "'reg'";
+}
+?>];
+//-->
+
+var cookie_prefix = '<?=$arResult["COOKIE_PREFIX"]?>';
+</script>
+<form method="post" name="form1" action="<?=$arResult["FORM_TARGET"]?>" >
+<?=$arResult["BX_SESSION_CHECK"]?>
+<input type="hidden" name="lang" value="<?=LANG?>" />
+<input type="hidden" name="ID" value=<?=$arResult["ID"]?> />
+
+
+<table class="profile-table data-table">
+	<tbody>
+	<?
+	if($arResult["ID"]>0)
+	{
+	?>
+		<?
+		if ($arResult["arUser"]["TIMESTAMP_X"] <> '')
+		{
+		?>
+
+			<td><?=GetMessage('LAST_UPDATE')?></td>
+			<td><?=$arResult["arUser"]["TIMESTAMP_X"]?></td>
+
+		<?
+		}
+		?>
+		<?
+		if ($arResult["arUser"]["LAST_LOGIN"] <> '')
+		{
+		?>
+		<?
+		}
+		?>
+	<?
+	}
+	?>
+	<tr>
+		<td class="title is-4"><?=GetMessage('LAST_NAME')?></td>
+		<td><input class="input is-info" type="text" name="LAST_NAME" maxlength="50" value="<?=$arResult["arUser"]["LAST_NAME"]?>" /></td>
+	</tr>
+	<tr>
+		<td class="title is-4"><?=GetMessage('NAME')?></td>
+		<td><input class="input is-info" type="text" name="NAME" maxlength="50" value="<?=$arResult["arUser"]["NAME"]?>" /></td>
+	</tr>
+	<tr>
+		<td class="title is-4"><?=GetMessage('LOGIN')?><span class="starrequired">*</span></td>
+		<td><input class="input is-info" type="text" name="LOGIN" maxlength="50" value="<? echo $arResult["arUser"]["LOGIN"]?>" /></td>
+	</tr>
+	<tr>
+		<td class="title is-4"><?=GetMessage('EMAIL')?><?if($arResult["EMAIL_REQUIRED"]):?><span class="starrequired">*</span><?endif?></td>
+		<td><input class="input is-info" type="text" name="EMAIL" maxlength="50" value="<? echo $arResult["arUser"]["EMAIL"]?>" /></td>
+	</tr>
+<?if($arResult['CAN_EDIT_PASSWORD']):?>
+	<tr>
+		<td class="title is-4"><?=GetMessage('NEW_PASSWORD_REQ')?></td>
+		<td>
+	<input title="<?echo $arResult["GROUP_POLICY"]["PASSWORD_REQUIREMENTS"];?>" class="input is-info" type="password" name="NEW_PASSWORD" maxlength="50" value="" autocomplete="off" class="bx-auth-input" />
+
+<?if($arResult["SECURE_AUTH"]):?>
+				<span class="bx-auth-secure" id="bx_auth_secure" title="<?echo GetMessage("AUTH_SECURE_NOTE")?>" style="display:none">
+					<div class="bx-auth-secure-icon"></div>
+				</span>
+				<noscript>
+				<span class="bx-auth-secure" title="<?echo GetMessage("AUTH_NONSECURE_NOTE")?>">
+					<div class="bx-auth-secure-icon bx-auth-secure-unlock"></div>
+				</span>
+				</noscript>
+<script type="text/javascript">
+document.getElementById('bx_auth_secure').style.display = 'inline-block';
+</script>
+		</td>
+	</tr>
+<?endif?>
+	<tr>
+		<td class="title is-4"><?=GetMessage('NEW_PASSWORD_CONFIRM')?></td>
+		<td>
+			<input title="<?echo $arResult["GROUP_POLICY"]["PASSWORD_REQUIREMENTS"];?>" class="input is-info" type="password" name="NEW_PASSWORD_CONFIRM" maxlength="50" value="" autocomplete="off" />
+			<p></p>
+		</td>
+	</tr>
+<?endif?>
+
+	</tbody>
+</table>
+</div>
+
+	<?// ********************* User properties ***************************************************?>
+	<?if($arResult["USER_PROPERTIES"]["SHOW"] == "Y"):?>
+	<div class="profile-link profile-user-div-link"><a title="<?=GetMessage("USER_SHOW_HIDE")?>" href="javascript:void(0)" onclick="SectionClick('user_properties')"><?=trim($arParams["USER_PROPERTY_NAME"]) <> '' ? $arParams["USER_PROPERTY_NAME"] : GetMessage("USER_TYPE_EDIT_TAB")?></a></div>
+	<div id="user_div_user_properties" class="profile-block-<?= mb_strpos($arResult["opened"], "user_properties") === false ? "hidden" : "shown"?>">
+	<table class="data-table profile-table">
+		<thead>
+			<tr>
+				<td colspan="2">&nbsp;</td>
+			</tr>
+		</thead>
+		<tbody>
+		<?$first = true;?>
+		<?foreach ($arResult["USER_PROPERTIES"]["DATA"] as $FIELD_NAME => $arUserField):?>
+		<tr><td class="field-name">
+			<?if ($arUserField["MANDATORY"]=="Y"):?>
+				<span class="starrequired">*</span>
+			<?endif;?>
+			<?=$arUserField["EDIT_FORM_LABEL"]?>:</td><td class="field-value">
+				<?$APPLICATION->IncludeComponent(
+					"bitrix:system.field.edit",
+					$arUserField["USER_TYPE"]["USER_TYPE_ID"],
+					array("bVarsFromForm" => $arResult["bVarsFromForm"], "arUserField" => $arUserField), null, array("HIDE_ICONS"=>"Y"));?></td></tr>
+		<?endforeach;?>
+		</tbody>
+	</table>
+	</div>
+	<?endif;?>
+	<?// ******************** /User properties ***************************************************?>
+
+	<div class="buttons" style="display: flex; justify-content: flex-end;">
+		<input class="button is-success" type="submit" name="save" value="<?=(($arResult["ID"]>0) ? GetMessage("MAIN_SAVE") : GetMessage("MAIN_ADD"))?>">&nbsp;&nbsp;
+		<input class="button is-warning" type="reset" value="<?=GetMessage('MAIN_RESET');?>">
+	</div>
+</form>
+<?
+if($arResult["SOCSERV_ENABLED"])
+{
+	$APPLICATION->IncludeComponent("bitrix:socserv.auth.split", ".default", array(
+			"SHOW_PROFILES" => "Y",
+			"ALLOW_DELETE" => "Y"
+		),
+		false
+	);
+}
+?>
+
+<?endif?>
+
+</div>
