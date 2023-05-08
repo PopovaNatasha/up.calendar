25bb7bafc0f54d27f852a0e7b87a662ef279c14b
Gleb <catinhat001@gmail.com>
2023-05-08 14:06:25 +0200

    Описание в инвайтах стало меньше

--- install/components/up/calendar.invite/templates/.default/template.php
+++ install/components/up/calendar.invite/templates/.default/template.php
@@ -7,7 +7,7 @@
     <div class="column is-5">
         <div class="box">
             <div class="title is-centered"><?= htmlspecialchars($arResult['TITLE']) ?></div>
-            <div class="title is-centered"><?= htmlspecialchars($arResult['DESCRIPTION']) ?></div>
+            <div class="title is-5 is-centered"><?= htmlspecialchars($arResult['DESCRIPTION']) ?></div>
             <form class="buttons" method="post">
                 <input type="hidden" name="link" value="<?= $arResult['link'] ?>"/>
                 <input type="hidden" name="action" value="in"/>
